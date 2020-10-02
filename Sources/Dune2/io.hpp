7e8ea5ef5aa940c20b22437c204d966c361e555f
Julien Graziano <julien@graziano.fr>
2020-10-03 01:13:37 +0200

    refactor: readInteger return a value

--- Sources/Dune2/io.hpp
+++ Sources/Dune2/io.hpp
@@ -9,17 +9,15 @@ namespace nr::dune2::io {
 /// readInteger
 /// generic function to read an integer on a input stream
 template<int N, typename IntType = uint32_t>
-std::optional<IntType>
-readInteger(std::istream &in) {
+IntType
+readInteger(std::istream &input) {
     static_assert(std::is_integral<IntType>::value, "Integral type required");
     static_assert(sizeof(IntType) >= N, "Integral type size too small");
 
     IntType value = IntType{0};
-    in.read(((char *)&value), N);
+    input.read(((char *)&value), N);
 
-    return in.gcount() == N
-        ? std::make_optional<IntType>(value)
-        : std::nullopt;
+    return value;
 }
 
 std::string readString(std::istream &);
