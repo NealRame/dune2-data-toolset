dc0ac5e47652bd1c679a5c14ca86bbcbf08dd7e5
Julien Graziano <julien@graziano.fr>
2020-10-10 02:50:26 +0200

    refactor(dune2): add `size` method

--- Sources/Dune2/palette.hpp
+++ Sources/Dune2/palette.hpp
@@ -11,6 +11,11 @@ class Palette {
 public:
     static std::optional<Palette> load(const std::string &filepath);
 
+public:
+    std::size_t size() const
+    { return colors_.size(); }
+
+    const Color &operator[](std::size_t index) const
 private:
     std::vector<Color> colors_;
 };
